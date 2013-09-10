5ecacac2b5037b4651290d3fb6767d6b20e448e2
gggeek <gg@ez.no>
2013-09-10 23:59:52 +0200

    activate phpdoc for daily docs

--- build/build_daily.sh
+++ build/build_daily.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-### update script for pubsvn: build docs from github sources
+### update script for pubsvn: build docs from github sources 
 ### @author g. giunta
 ### @version $Id$
 
 # modified on 2013.5.2: we only rebuild trunk every nite, using pakefile
 
 PHP=/usr/local/php-5.4.19/bin/php
-TOOLS="doxygen sami"
+TOOLS="doxygen sami phpdoc"
 DOCSROOT=../www/trunk
 
 cd ezpublishbuilder
@@ -33,7 +33,7 @@ cd ezpublishbuilder
 # this avoids the very verbose output from pake when doing the equivalent thing
 rm -rf build/sami_cache
 
-# note: we have to use 4X here and not LS because repo layout is nto the same as release layout
+# note: we have to use 4X here and not LS because repo layout is not the same as release layout
 $PHP pakefile.php generate-apidocs-4X --user-config-file=../options-ezpublish-user.yaml --sourcedir=../source/trunk/legacy --docsdir=../../www/trunk/LS --option.docs.include_sources=1 --option.version.alias=Trunk
 
 rm -rf build/sami_cache
